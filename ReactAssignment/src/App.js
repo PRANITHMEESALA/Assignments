93da62c49b7efd610352575a81a787282c32f808
PRANITHMEESALA <47515348+PRANITHMEESALA@users.noreply.github.com>
2020-05-06 17:42:36 +0530

    Update

--- ReactAssignment/src/App.js
+++ ReactAssignment/src/App.js
@@ -8,14 +8,7 @@ class App extends Component {
 
 
     
-    // componentDidMount(){
-    //     axios.get('http://dummy.restapiexample.com/api/v1/employees')
-    //     .then(resp => {
-    //         this.setState({emp:resp.data} );
-    //      // console.log(resp);  
-    //     });
-        
-    //   }(
+  
 OnclickHandler=()=>{
     
     this.setState({Name:""})
@@ -30,7 +23,7 @@ OnclickHandler=()=>{
            return (
 
                <div>
-                
+
                    
                    <Control/>
                 
